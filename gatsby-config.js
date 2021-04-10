97d3a0401356d34b8057a7482d33733fccee3c83
Hiroharu Nakama <haru@nakama.local>
2021-04-11 07:37:54 +0900

    modify: site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     siteMetadata: {
-        title: `コードクッキング - JavaScript`,
+        title: `コードクッキング - JavaScriptのコードレシピ`,
         author: {
             name: `Haru Nakama`,
         },
